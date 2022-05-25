1e75c83a5d4a43761c87f9775aa3a2831f2b02d8
George Wood <george.wood@nyu.edu>
2022-05-25 18:38:57 -0400

    Add latest pipeline

--- run.R
+++ run.R
@@ -2,5 +2,14 @@
 targets::tar_make()
 
 # latest pipeline
-htmltools::save_html(html = targets::tar_visnetwork(),
-                     file = "pipeline.html")
+htmltools::save_html(
+  html = targets::tar_visnetwork(),
+  file = "pipeline.html"
+)
+webshot::webshot(
+  "pipeline.html",
+  selector = "body",
+  zoom = 10,
+  file = "pipeline.png"
+)
+
